6556fbe8c5ccea734cfc779fe360325b798481a9
Prelang Builder <builder@prelang.com>
2014-07-04 10:04:16 +0000

    Added require_user_signed_in filters to scaffolded controller

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,4 +1,8 @@
 class StoriesController < ApplicationController
+
+  #->Prelang (scaffolding:rails/scope_to_user)
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
   before_action :set_story, only: [:show, :edit, :update, :destroy]
 
   # GET /stories
